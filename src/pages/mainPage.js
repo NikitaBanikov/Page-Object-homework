02476e68799d141d7f32350651e2db244297d211
Никита <nikitabannikov@bk.ru>
2025-10-14 10:28:06 +0300

    refactoring fix

--- src/pages/mainPage.js
+++ src/pages/mainPage.js
@@ -1,9 +1,9 @@
 export class MainPage {
-
     constructor(page) {
         this.page = page;
         this.globalFeedTab = page.locator('button:has-text("Global Feed")');
         this.articlePreview = page.locator('.article-preview');
+        this.articleTitle = page.locator('.container h1');
     }
 
     async goto() {
@@ -24,5 +24,8 @@ export class MainPage {
         await likeButton.click();
         return likeButton.locator('.counter');
     }
-    
+
+    async isArticleVisible(title) {
+        return await this.page.getByText(title).isVisible();
+    }
 }
